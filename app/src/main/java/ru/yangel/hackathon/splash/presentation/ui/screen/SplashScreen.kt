31c22f5a82e2ee6890091b43366bbb237e5a1877
Yuriy Yangel <arlosaneq@gmail.com>
2024-04-20 10:33:19 +0700

    Added font

---
+++ app/src/main/java/ru/yangel/hackathon/splash/presentation/ui/screen/SplashScreen.kt
@@ -0,0 +1,34 @@
+package ru.yangel.hackathon.splash.presentation.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.yangel.hackathon.R
+
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.cake_logo),
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp),
+            contentDescription = null
+        )
+    }
+}
