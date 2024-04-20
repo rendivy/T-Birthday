1f452400f0f3d95c424ec8d140c1c4f24079c613
Suslanium <zaharov22m@gmail.com>
2024-04-20 11:45:56 +0700

    Core UI components

---
+++ app/src/main/java/ru/yangel/hackathon/ui/theme/Dimens.kt
@@ -0,0 +1,5 @@
+package ru.yangel.hackathon.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val PaddingMedium = 16.dp
--- app/src/main/java/ru/yangel/hackathon/ui/theme/Font.kt
+++ app/src/main/java/ru/yangel/hackathon/ui/theme/Font.kt
@@ -1,5 +1,6 @@
 package ru.yangel.hackathon.ui.theme
 
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -15,6 +16,48 @@ internal val FontSmall = 16.sp
 internal val FontScheduleTitle = 23.sp
 
 val Roboto = FontFamily(
-    Font(R.font.roboto_flex),
-    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_flex, FontWeight.SemiBold),
+    Font(R.font.roboto_regular, FontWeight.Normal)
+)
+
+val Type24 = TextStyle(
+    fontFamily = Roboto,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    lineHeight = 28.sp
+)
+
+val Type20 = TextStyle(
+    fontFamily = Roboto,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 20.sp,
+    lineHeight = 28.sp
+)
+
+val Type15 = TextStyle(
+    fontFamily = Roboto,
+    fontWeight = FontWeight.Normal,
+    fontSize = 15.sp,
+    lineHeight = 24.sp
+)
+
+val Type13 = TextStyle(
+    fontFamily = Roboto,
+    fontWeight = FontWeight.Normal,
+    fontSize = 13.sp,
+    lineHeight = 20.sp
+)
+
+val Type11 = TextStyle(
+    fontFamily = Roboto,
+    fontWeight = FontWeight.Normal,
+    fontSize = 11.sp,
+    lineHeight = 16.sp
+)
+
+val Type10 = TextStyle(
+    fontFamily = Roboto,
+    fontWeight = FontWeight.Normal,
+    fontSize = 10.sp,
+    lineHeight = 20.sp
 )
